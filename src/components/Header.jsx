4338750cdfc549df45c71808b4abc7e3ce7e4675
Andrey Bodro <38718771+1bodro@users.noreply.github.com>
2020-04-18 21:27:07 +0300

    lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://image.freepik.com/free-photo/man-through-hole-paper_1368-47349.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Sidebar = () => {
+  return (
+    <nav className="sidebar">
+      <span className="sidebar__link">Profile</span>
+      <span className="sidebar__link">Message</span>
+      <span className="sidebar__link">News</span>
+      <span className="sidebar__link">Music</span>
+      <span className="sidebar__link">Settings</span>
+    </nav>
+  );
+};
