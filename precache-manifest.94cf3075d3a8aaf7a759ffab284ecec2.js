275096d01ed47fa3add522eaac707b5cbcc204df
bodro <bodro@syneforge.com>
2021-01-10 18:04:56 +0200

    Updates

---
+++ precache-manifest.94cf3075d3a8aaf7a759ffab284ecec2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c118a72afc4e84207b48",
+    "url": "/reactwarriors-master-class/static/css/4.004a731c.chunk.css"
+  },
+  {
+    "revision": "5ecf008806235fb6f18a",
+    "url": "/reactwarriors-master-class/static/css/main.1abb9bff.chunk.css"
+  },
+  {
+    "revision": "98388ee980faa179013f",
+    "url": "/reactwarriors-master-class/static/js/runtime~main.03111cb9.js"
+  },
+  {
+    "revision": "3b2b1eec01e586b4a688ac62b5c5758c",
+    "url": "/reactwarriors-master-class/static/media/logo.3b2b1eec.jpg"
+  },
+  {
+    "revision": "6b62a8d79afa2bde86cb",
+    "url": "/reactwarriors-master-class/static/js/2.900f120c.chunk.js"
+  },
+  {
+    "revision": "d5ec859f21d471632d016de3ebe25944",
+    "url": "/reactwarriors-master-class/static/media/beach.d5ec859f.jpg"
+  },
+  {
+    "revision": "e058023d5bfe3023c065",
+    "url": "/reactwarriors-master-class/static/js/3.0f2272aa.chunk.js"
+  },
+  {
+    "revision": "5ecf008806235fb6f18a",
+    "url": "/reactwarriors-master-class/static/js/main.51e220a0.chunk.js"
+  },
+  {
+    "revision": "c118a72afc4e84207b48",
+    "url": "/reactwarriors-master-class/static/js/4.04f99837.chunk.js"
+  },
+  {
+    "revision": "6fd2c60b832caad93fb712f959f828e2",
+    "url": "/reactwarriors-master-class/static/media/loading.6fd2c60b.svg"
+  },
+  {
+    "revision": "d90e6bbe1212d6160155",
+    "url": "/reactwarriors-master-class/static/js/5.6f06d6d2.chunk.js"
+  },
+  {
+    "revision": "d90e6bbe1212d6160155",
+    "url": "/reactwarriors-master-class/static/css/5.230525ac.chunk.css"
+  },
+  {
+    "revision": "e058023d5bfe3023c065",
+    "url": "/reactwarriors-master-class/static/css/3.c726ff64.chunk.css"
+  },
+  {
+    "revision": "6b62a8d79afa2bde86cb",
+    "url": "/reactwarriors-master-class/static/css/2.ce452ec0.chunk.css"
+  },
+  {
+    "revision": "d2adb868a3ac33fff15a01ed4ffee796",
+    "url": "/reactwarriors-master-class/index.html"
+  }
+];
